0402fa76c7b617b392e133352debcd3f1ff9b58a
Sergio Mota <sergio.mota2514@gmail.com>
2021-05-26 15:15:21 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller } from '@nestjs/common';
+import {
+  MessagePattern,
+  RmqContext,
+  Ctx,
+  Payload,
+} from '@nestjs/microservices';
+@Controller()
+export class AppController {
+  constructor() {}
+  @MessagePattern('rabbit-mq-producer')
+  public async execute(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log('data', data);
+    const channel = context.getChannelRef();
+    console.log('channel', channel);
+    const orginalMessage = context.getMessage();
+    console.log('orginalMessage', orginalMessage);
+    channel.ack(orginalMessage);
+  }
+}
