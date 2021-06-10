f983d75002387901fc2fa55e78cbe826377ef02e
Sergio Mota <sergio.mota2514@gmail.com>
2021-06-10 20:07:39 -0300

    moving the configuration to modules file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'rabbit-mq-module',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_ADDRESS],
+          queue: process.env.RABBITMQ_QUEUE,
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  const configRabbit = {
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_ADDRESS],
-      queue: process.env.RABBITMQ_QUEUE,
-      queueOptions: {
-        durable: false,
-      },
-      // false = manual acknowledgement; true = automatic acknowledgment
-      noAck: false,
-      // Get one by one
-      prefetchCount: 1,
-    },
-  };
-  const app = await NestFactory.createMicroservice(AppModule, configRabbit);
+  const app = await NestFactory.createMicroservice(AppModule);
   await app.listenAsync();
 }
 bootstrap();
